164578ed3fd52359a9d3fd5db7165b514b3f5dc4
YF Looi <yihfoo@gmail.com>
2022-08-22 23:51:34 +0800

    Initial docker-compose setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # local-docker-microservice-setup
 Run multiple repos as microservices on local using docker-compose
+
+# Start command
+`docker compose -f docker-compose.local-microservices.yml --env-file ./.env up`
+
+For older docker-compose installations, replace `docker compose` with `docker-compose`
--- operations-portal/deployment/scripts/app-dev-restart.sh
+++
@@ -1,4 +0,0 @@
-DEV_CLUSTER="gke-my-deployment-cluster"
-kubectl delete service operations-portal-service --namespace=dev --cluster=${DEV_CLUSTER}
-kubectl delete deployment operations-portal-deployment --namespace=dev --cluster=${DEV_CLUSTER}
-kubectl apply -f ./deployment/k8s/dev/deployment-dev.yaml --namespace=dev --cluster=${DEV_CLUSTER}
